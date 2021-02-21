7e00fc98098d81e85544e29aeaf45ab60f4314cf
RomanaManuel <mr.romanamanuel@gmail.com>
2021-02-22 00:25:28 +0100

    try script

---
+++ scripts/deploy.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+
+echo $GCLOUD_SERVICE_KEY > ./key_file.json
+gcloud auth activate-service-account --key-file=key_file.json
+gcloud config set core/project $GOOGLE_PROJECT_ID 
+gcloud config set compute/zone $GOOGLE_COMPUTE_ZONE
+CLUSTER_EXIST = gcloud container cluster list $(gcloud container clusters list --format="value(name)" --filter="name=$GOOGLE_CLUSTER_NAME")
+
+if [$CLUSTER_EXIST != $GOOGLE_CLUSTER_NAME] ;then 
+
+    gcloud container cluster create $GOOGLE_CLUSTER_NAME
+
+else
+    echo $CLUSTER_EXIST
+
