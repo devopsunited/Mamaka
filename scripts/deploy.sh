abc259b90e270cfacd739c304b35f5cca899022f
RomanaManuel <mr.romanamanuel@gmail.com>
2021-02-22 01:24:01 +0100

    script.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,13 +5,13 @@ echo $GCLOUD_SERVICE_KEY | base64 --decode --ignore-garbage > key_file.json
 gcloud auth activate-service-account --key-file=key_file.json
 gcloud config set core/project $GOOGLE_PROJECT_ID 
 gcloud config set compute/zone $GOOGLE_COMPUTE_ZONE
-CLUSTER_EXIST = gcloud container cluster list $(gcloud container clusters list --format="value(name)" --filter="name=$GOOGLE_CLUSTER_NAME")
+CLUSTER_EXIST =$(gcloud container clusters list --format="value(name)" --filter="name=$GOOGLE_CLUSTER_NAME")
 TEST="TOTO"
 echo $TEST
 echo $GOOGLE_CLUSTER_NAME
 echo "cluster exist ? ${CLUSTER_EXIST}"
 if [ "${CLUSTER_EXIST}" != $GOOGLE_CLUSTER_NAME ] ;then 
-    
+
    gcloud container clusters create hellocluster
 
 else
